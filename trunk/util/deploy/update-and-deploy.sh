6b4ef9a68c1c5f9107fb9b1bac73f32f52591f1f
victorsophieparis@gmail.com <victorsophieparis@gmail.com@e85c92d7-f80b-48a6-50b3-5736d7b4cf47>
2013-02-04 04:30:02 +0000

    Victor: Added DB structure + sp and deployment scripts

--- trunk/util/deploy/update-and-deploy.sh
+++ trunk/util/deploy/update-and-deploy.sh
@@ -3,4 +3,4 @@
 SVN_REPO=/home/sos/svn
 
 $SVN_REPO/trunk/util/deploy/update.sh $SVN_REPO/trunk
-$SVN_REPO/trunk/util/deploy/deploy-to-dev.sh $SVN_REPO/trunk 
+$SVN_REPO/trunk/util/deploy/deploy.sh $SVN_REPO/trunk 
