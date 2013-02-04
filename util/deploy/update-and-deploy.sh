3cd169e9e6f2fe55e40b7c498e8d35f0286ac43d
victorsophieparis <victorsophieparis@gmail.com>
2013-02-04 08:38:17 +0000

    Victor: Updated deployment scripts

--- util/deploy/update-and-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SVN_REPO=/home/sos/svn
-
-$SVN_REPO/trunk/util/deploy/update.sh $SVN_REPO/trunk
-$SVN_REPO/trunk/util/deploy/deploy.sh $SVN_REPO/trunk 
--- util/deploy/update.sh
+++ util/deploy/update.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+WWWROOT=$1
+REV=$2
 
-SVN_REPO=$1
+URLREPO="http://esophie-soslab.googlecode.com/svn/trunk"
 
-echo '***************** UPDATING REPOSITORY ****************'
-cd $SVN_REPO
-sudo -u sos svn update
+echo "***************** UPDATING DEPLOYMENT SCRIPTS *****"
+echo "Deployment scripts REV $REV -> $WWWROOT/scripts/deploy-$REV"
+if [ ! -f "$WWWROOT/scripts/deploy-$REV" ];
+then
+    sudo -u sos svn export --force -r $REV $URLREPO/util/deploy $WWWROOT/scripts/deploy-$REV
+else
+    echo "Deployment script already up-to-date - no need to update."
+fi
