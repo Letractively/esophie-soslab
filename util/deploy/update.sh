c49e2cad3c7136a5508e8a640964f5b09f83cc2a
victorsophieparis <victorsophieparis@gmail.com>
2013-06-24 11:58:47 +0000

    Victor: changed deployment user to www-data

--- util/deploy/update.sh
+++ util/deploy/update.sh
@@ -8,7 +8,7 @@ echo "***************** UPDATING DEPLOYMENT SCRIPTS *****"
 echo "Deployment scripts REV $REV -> $WWWROOT/scripts/deploy-$REV"
 if [ ! -f "$WWWROOT/scripts/deploy-$REV" ];
 then
-    sudo -u sos svn export --force -r $REV $URLREPO/util/deploy $WWWROOT/scripts/deploy-$REV
+    sudo -u www-data svn export --force -r $REV $URLREPO/util/deploy $WWWROOT/scripts/deploy-$REV
 else
     echo "Deployment script already up-to-date - no need to update."
 fi
