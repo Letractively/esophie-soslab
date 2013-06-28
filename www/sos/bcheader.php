41ed92bc92b978030e93d705bc7ac1fe87565cf9
victorsophieparis <victorsophieparis@gmail.com>
2013-06-28 13:21:18 +0000

    Victor: installed google analytics - 2

--- www/sos/bcheader.php
+++ www/sos/bcheader.php
@@ -2,17 +2,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ind" xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	<title>Sophie Online Shopping</title>
-	<meta name="viewport" content="width=360;"> 
-	<link rel="icon" type="image/ico" href="images/favicon.ico">
-	<link type="text/css" rel="stylesheet" href="css/global.css">
-	<link type="text/css" rel="stylesheet" href="css/calendar.css">
-	<script language="javascript" src="script/global.js"></script>
-	<script language="javascript" src="script/calendar.js"></script>
-	</head>
-	<body>
-        <script>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+	<title>Sophie Online Orders - Backoffice BC</title>
+	<meta name="viewport" content="width=360;"/> 
+	<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+	<link type="text/css" rel="stylesheet" href="css/global.css"/>
+	<link type="text/css" rel="stylesheet" href="css/calendar.css"/>
+	<script language="Javascript" type="text/javascript" src="script/global.js"/>
+	<script language="Javascript" type="text/javascript" src="script/calendar.js"/>
+        <script type="text/javascript">
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -22,6 +20,8 @@
             ga('send', 'pageview');
 
         </script>
+	</head>
+	<body>
 	<form id="frmmain" name="frmmain" action="<?=$ctrl->filename()?>" method="post">
 		<center>		
 		<img src="images/logo.png" class="logo" alt="Sophie Online Shopping"/>
--- www/sos/mbrheader.php
+++ www/sos/mbrheader.php
@@ -3,14 +3,12 @@
 <html lang="ind" xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
-	<title>Sophie Online Shopping</title>
+	<title>Sophie Mobile - Online Orders</title>
 	<meta name="viewport" content="width=360;"/> 
 	<link rel="icon" type="image/ico" href="images/favicon.ico"/>
 	<link type="text/css" rel="stylesheet" href="css/globalmobile.css"/>
 	<script language="Javascript" type="text/javascript" src="script/global.js"/>
-	</head>
-	<body class="mobile">  
-        <script>
+        <script type="text/javascript">
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -19,7 +17,9 @@
           ga('create', 'UA-42095429-1', 'sophiemobile.com');
           ga('send', 'pageview');
 
-        </script>          
+        </script>  
+	</head>
+	<body class="mobile">     
 	<form id="frmexec" method="post"></form>
 	<form id="frmmain" action="<?=$ctrl->filename()?>" method="post">
 		<center>
