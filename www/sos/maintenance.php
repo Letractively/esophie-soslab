6d4a054df60a63b871ea23ad3d4993f3c05093d2
victorsophieparis <victorsophieparis@gmail.com>
2013-09-30 14:01:43 +0000

    Victor: modified maintenance page + completed monitoring job (sql + cron)

--- www/sos/maintenance.php
+++ www/sos/maintenance.php
@@ -1,24 +1,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="ind" xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Sophie Mobile - Online Orders</title>
-    <meta name="viewport" content="width=360"/> 
-    <link rel="icon" type="image/ico" href="images/favicon.ico"/>
-    <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-precomposed.png" />
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
-    <link type="text/css" rel="stylesheet" href="css/globalmobile.css"/>
-    <script type="text/javascript">
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-42095429-1', 'sophiemobile.com');
-      ga('send', 'pageview');
-
-    </script>  
-    </head>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Sophie Mobile - Online Orders</title>
+	<meta name="viewport" content="width=360"/> 
+	<link rel="icon" type="image/ico" href="images/favicon.ico"/>
+        <link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon-precomposed.png" />
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png" />
+	<link type="text/css" rel="stylesheet" href="css/globalmobile.css"/>
+        <? include_once "mbranalytics.php"; ?> 
+        <script type="text/javascript" src="script/global.js?v3"></script>
+	</head>
     <body class="mobile">     
         <div class="imglogo">
             <img src="images/logo.png" class="logo" alt="Sophie Online Shopping"/>
@@ -26,11 +18,7 @@
         <div class="boxmain" style="width:340px;">
             <p class="pink">We&rsquo;ll be back soon!</p>
             <div style="text-align:left;margin:0 20px;">
-                <p>Berkaitan dengan libur nasional Hari Raya Idul Fitri 1 Syawal 1434 H maka Sophie Online Order 
-                    untuk sementara waktu tidak akan berfungsi. Hal ini dikarenakan oleh jaringan bank yang akan 
-                    offline mulai tanggal 5 – 12 Agustus 2013. Sophie Online Order dapat dipergunakan lagi pada 
-                    tanggal 13 Agustus 2013.</p>
-                <p>Atas perhatian dan kerjasama yang baik , kami ucapkan terima kasih.</p>
+                <p>Maaf, untuk sementara layanan Online Order kami sedang offline. Atas perhatian dan kerjasama yang baik , kami ucapkan terima kasih.</p>
                 <p class="italicpink">&mdash; Sophie Paris</p>
             </div>
             <a href="http://sophiemobile.com">Kembali ke Sophie Mobile</a>
