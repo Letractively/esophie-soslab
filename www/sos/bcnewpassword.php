358e16d191235395ec169455b743c5ebadd2126d
victorsophieparis <victorsophieparis@gmail.com>
2013-10-23 09:18:32 +0000

    Victor: BC can choose its own password

--- www/sos/bcnewpassword.php
+++ www/sos/bcnewpassword.php
@@ -1,25 +1,33 @@
 <?include "bcheader.php";?>
 
-<div class="boxcon" style="width:250px; margin:10px; padding:10px; text-align:center;">
+<div class="boxcon" style="margin:10px; padding:10px; text-align:center;">
 <?if ($ctrl->pageindex == 1) { ?>
-<b>Silahkan masukkan:</b>
+Silahkan merubah password Anda di sini::
 <br/><br/>
-<table style="width:230px; text-align:right;">
+<table style="width:100%; text-align:right;">
     <tr><td>Kode BC:</td>
-    <td><div class="boxstyled1" style="width:100%"><input type="text" name="userid" id="userid" value="" placeholder="Kode BC" style="width:150px"/></div></td></tr>
+    <td><div class="boxstyled1" style="width:100%"><input type="text" name="userid" id="userid" value="<?=$ctrl->value("userid")?>" placeholder="Kode BC" style="width:150px"/></div></td></tr>
     <tr><td>Email:</td>
     <td><div class="boxstyled1" style="width:100%"><input type="email" name="email" id="email" value="<?=$ctrl->value("email")?>" placeholder="Email address" style="width:150px"/></div></td></tr>
+    <tr><td>Password lama:</td>
+    <td><div class="boxstyled1" style="width:100%"><input type="password" name="oldpwd" id="oldpwd" value="<?=$ctrl->value("oldpwd")?>" placeholder="Old password" style="width:150px"/></div></td></tr>
+    <tr><td>&nbsp;</td><td>&nbsp;</td></tr>
+    <tr><td>Password baru x1:</td>
+    <td><div class="boxstyled1" style="width:100%"><input type="password" name="newpwd1" id="newpwd1" value="" placeholder="New password" style="width:150px"/></div></td></tr>
+    <tr><td>Password baru x2:</td>
+    <td><div class="boxstyled1" style="width:100%"><input type="password" name="newpwd2" id="newpwd2" value="" placeholder="New password (again)" style="width:150px"/></div></td></tr>
+
 </table>
 <br/>
-<input type="submit" onclick="setaction('ok');" class="buttongo" style="width:180px;" value="Kirim password baru" />
+<input type="submit" onclick="setaction('ok');" class="buttongo" style="width:180px;" value="Ganti password" />
 <br/><br/>
 <? if ($ctrl->varvalue("errmsg") != '') { ?>
         <div class="errormessage"><?=$ctrl->varvalue("errmsg")?></div>
 <? } ?>
-<a href="bclogin.php"><< kembali ke layar login</a>
+<a href="bclogin.php"><< Kembali ke layar login</a>
 <? } else { ?>
-	Password baru sudah dikirimkan ke email anda.<br>
-	silahkan cek email anda beberapa menit kedepan.<br>
+	<br>Password Anda sudah dirubah dan dikirimkan ke alamat email Anda.<br><br>
+	Silahkan <a href="bclogin.php"> kembali ke layar login</a><br><br>
 <? } ?>
 </div>
 <?include "bcfooter.php";?>
